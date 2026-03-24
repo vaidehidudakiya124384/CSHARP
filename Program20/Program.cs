7db0b3e08e67b01572e7943e5f1b01557bff3c77
vaidehidudakiya124384 <vaidehi.dudakiya124384@marwadiuniversity.ac.in>
2026-03-24 07:24:07 +0530

    Add files via upload

---
+++ Program20/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            r.Calculate();
+            r.Display();
+            Rectangle r2 = new Rectangle(30,40);
+            r2.Calculate();
+            r2.Display();
+        }
+    }
+}
---
+++ Program20/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program20
+{
+    internal class Rectangle
+    {
+        int l, w, area;
+        public Rectangle()
+        {
+            l = 10;
+            w = 20;
+        }
+        public Rectangle(int l1, int w1)
+        {
+            l = l1;
+            w = w1;
+        }
+        public void Calculate()
+        {
+            area = l * w;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Area:" + area);
+        }
+    }
+}
