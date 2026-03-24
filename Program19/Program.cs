c4ad7331e22d2c5f1ff8b9513bf8ba93682b2362
vaidehidudakiya124384 <vaidehi.dudakiya124384@marwadiuniversity.ac.in>
2026-03-24 07:22:28 +0530

    Add files via upload

---
+++ Program19/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[5];
+            int min;
+            Console.WriteLine("Enter 5 numbers:");
+
+            for (int i = 0; i < 5; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            min = a[0];
+            for (int i = 1; i < 5; i++)
+            {
+                if (a[i] < min)
+                {
+                    min = a[i];
+                }
+            }
+            Console.WriteLine("Maximum number =" + min);
+        }
+    }
+}
